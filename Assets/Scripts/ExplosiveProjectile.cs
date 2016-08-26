1d0c0352bcf5e8d6e671fe78fb99103b2ab15685
Mark Feaver <mark.feaver@gmail.com>
2016-08-26 23:17:06 +1200

    Added glitch effect if looking at grenade when it explodes (after 3 seconds)

---
+++ Assets/Scripts/ExplosiveProjectile.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosiveProjectile : MonoBehaviour {
+
+	public float fuseTime = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fuseTime -= Time.deltaTime;
+		if (fuseTime < 0.0f) {
+			Explode ();
+		}
+	}
+
+	void Explode () {
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+		foreach (GameObject player in players) {
+			player.GetComponent<PlayerGlitch> ().ProjectileExploded(this);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerGlitch.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kino;
+
+public class PlayerGlitch : MonoBehaviour {
+
+	#region Public Properties
+
+	public Camera myCamera;
+	public Datamosh datamosh;
+	public float maximumGlitchTime = 5.0f;
+
+	#endregion
+
+	#region Private Properties
+
+	private float glitchTimeRemaining = 0.0f;
+
+	#endregion
+
+	#region Unity Methods
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	#endregion
+
+	#region Public Methods
+
+	public void ProjectileExploded (ExplosiveProjectile projectile) {
+		// Check to see if the projectile is visible
+		Vector3 screenPoint = myCamera.WorldToViewportPoint (projectile.transform.position);
+		if (screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1) {
+			EnableGlitch ();
+		}
+	}
+
+	#endregion
+
+	#region Private Methods
+
+	void EnableGlitch () {
+		StartCoroutine (GlitchMe ());
+	}
+
+	IEnumerator GlitchMe () {
+		glitchTimeRemaining = maximumGlitchTime;
+		datamosh.entropy = 1.0f;
+		datamosh.Glitch ();
+		while (glitchTimeRemaining > 0.0f) {
+			glitchTimeRemaining -= Time.deltaTime;
+			datamosh.entropy = glitchTimeRemaining / maximumGlitchTime;
+			yield return null;
+		}
+		datamosh.entropy = 1.0f;
+		datamosh.Reset ();
+	}
+
+	#endregion
+}
