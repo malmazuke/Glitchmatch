35a8d60be373db263affcdffe970a29f9c276acf
Mark Feaver <mark.feaver@gmail.com>
2016-08-26 21:54:12 +1200

    Made a test scene

---
+++ Assets/Scripts/GlitchOverTime.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlitchOverTime : MonoBehaviour {
+
+	private Kino.Datamosh mosh;
+
+	// Use this for initialization
+	void Start () {
+		mosh = GetComponent<Kino.Datamosh> ();
+		mosh.Glitch ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mosh.entropy += 0.001f;
+		if (mosh.entropy > 1) {
+			mosh.entropy -= 1.0f;
+		}
+	}
+}
