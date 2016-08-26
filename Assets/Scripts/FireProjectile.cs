9084209782b702a7dc58ee18f343203f7281adc1
Mark Feaver <mark.feaver@gmail.com>
2016-08-26 22:31:19 +1200

    Added grenade firing

---
+++ Assets/Scripts/FireProjectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireProjectile : MonoBehaviour {
+
+	public GameObject projectilePrefab;
+	public Transform projectileSpawn;
+	public float projectileForce = 20.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		HandleInput ();
+	}
+
+	void HandleInput () {
+		if (Input.GetButtonDown ("Fire1")) {
+			Fire ();
+		}
+	}
+
+	void Fire () {
+		GameObject projectile = (GameObject)GameObject.Instantiate(projectilePrefab, projectileSpawn.position, projectileSpawn.rotation);
+		Rigidbody rb = projectile.GetComponent<Rigidbody> ();
+
+		rb.AddForce (projectile.transform.forward * projectileForce);
+	}
+}
