01f9d26dea211bd6a3746cf335600b7b2728e6ad
Mark Feaver <mark.feaver@gmail.com>
2016-08-27 11:08:04 +1200

    Created a floaty collectable

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Collectable : MonoBehaviour {
+
+	#region Public Properties
+
+	public float waveSpeed = 3.0f;
+	public float waveAmplitude = 0.1f;
+
+	#endregion
+
+	#region Private Properties
+
+	Vector3 originalPosition;
+
+	#endregion
+
+	#region Unity Methods
+
+	void Awake () {
+		originalPosition = transform.position;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 updatedPosition = transform.position;
+		updatedPosition.y = originalPosition.y +  Mathf.Sin (Time.time * waveSpeed) * waveAmplitude;
+		transform.position = updatedPosition;
+
+		transform.Rotate (1.0f, 0.0f, -1.0f);
+	}
+
+	#endregion
+}
