9d27f712a662c7a3a4865d7f7615ff3211b84861
JimRush <jim.rush@homes.co.nz>
2016-08-28 12:56:28 +1200

    Added title screen controller script to call dataglitch.

---
+++ Assets/TitleScreenController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Kino;
+
+public class TitleScreenController : MonoBehaviour {
+
+	private Datamosh dm; 
+	// Use this for initialization
+	void Start () {
+		dm = GetComponent<Datamosh> ();
+		dm.Glitch();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
